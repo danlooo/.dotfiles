95ebc1293ae76bd2ad9a87e3dc5eacd631f14247
danlooo <loosdaniel@gmail.com>
2022-07-10 12:26:39 +0200

    Add aliases make and open

--- home/.aliases.sh
+++ home/.aliases.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
+alias m='make'
 alias v='vim'
 alias c='code'
 alias d='docker'
+alias o='xdg-open'
+alias open='xdg-open'
 alias g='git'
 alias l='ls --color=tty'
 alias ll='ls -lah --color=tty'
